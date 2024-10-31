850a8936c7aa67f230e98b88c714abe0438e27be
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2024-10-31 20:22:57 -0300

    :bricks: ci: Modificação no server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/tasks", taskRoutes);
+app.use("/api/tasks", taskRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URI, {
